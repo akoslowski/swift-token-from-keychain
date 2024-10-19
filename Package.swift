46a01f9753cabd7f4125953266e9451c34d893a5
akoslowski <2005824+akoslowski@users.noreply.github.com>
2024-10-19 17:42:39 +0200

    Change name of package

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "TokenFromKeychain",
+    name: "swift-token-from-keychain",
     products: [
         .library(
             name: "TokenFromKeychain",
